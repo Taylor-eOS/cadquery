23a6f461c84812534f4d846d9670d83477d021c1
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-05-15 21:04:06 +0200

    Added ribs
    No gussets yet.

--- esp32_protector/protector.py
+++ esp32_protector/protector.py
@@ -4,8 +4,8 @@ m = 0.1
 pcb_x = 23.0 + (2 * m)
 pcb_y = 18.0 + (2 * m)
 bottom_z = 5.3
-top_height = 2.7
-wall_z = bottom_z + top_height
+top_z = 2.7
+tot_plate_z = bottom_z + top_z
 wall_thickness = 1.0
 case_x = pcb_x + (2 * wall_thickness)
 case_y = pcb_y + (2 * wall_thickness)
@@ -16,60 +16,81 @@ gap_width = 8.0
 c = (True, True, False)
 pin_hole_x = 20.0 + (2 * m)
 pin_hole_y = 2.4 + (2 * m)
-pin_hole_x_offset = -0.4
+pin_hole_x_offset = -0.7
 pin_y_offset = (pcb_y / 2) - (pin_hole_y / 2)
-usb_h = 2.4 + (2 * m)
-usb_w = 8.3 + (2 * m)
-usb_r = (usb_h / 2) - 0.01
-usb_z = wall_z + 0.7
+usb_z = 3.0 + (2 * m)
+usb_y = 8.8 + (2 * m)
+usb_r = (usb_z / 2) - m
+usb_x = tot_plate_z + 0.7
+rib_x = 10.0
+rib_y = 0.2
+rib_z = top_z
+rib_inner_front_y = (pcb_y / 2) - (rib_y / 2)
+rib_inner_back_y = -(pcb_y / 2) + (rib_y / 2)
 
 def make_short_wall(tx):
     return (
         cq.Workplane("XY")
-        .box(wall_thickness, case_y, top_height, centered=c)
-        .translate((tx, 0, wall_z))
+        .box(wall_thickness, case_y, top_z, centered=c)
+        .translate((tx, 0, tot_plate_z))
     )
 
 def make_long_wall(ty):
     return (
         cq.Workplane("XY")
-        .box(pcb_x, wall_thickness, top_height, centered=c)
-        .translate((0, ty, wall_z))
+        .box(pcb_x, wall_thickness, top_z, centered=c)
+        .translate((0, ty, tot_plate_z))
     )
 
 def make_gap(ty):
     return (
         cq.Workplane("XY")
-        .box(gap_width, wall_thickness * 3, top_height * 2, centered=c)
+        .box(gap_width, wall_thickness * 3, top_z * 2, centered=c)
         .translate((0, ty, bottom_z))
     )
 
-bottom_plate = (
-    cq.Workplane("XY")
-    .box(case_x, case_y, bottom_z, centered=c)
-)
+def make_crush_rib(ty):
+    return (
+        cq.Workplane("XY")
+        .box(rib_x, rib_y, rib_z, centered=c)
+        .translate((0, ty, tot_plate_z))
+    )
 
-top_plate = (
-    cq.Workplane("XY")
-    .box(case_x, case_y, top_height, centered=c)
-    .translate((0, 0, bottom_z))
-)
+def make_usb_hole():
+    return (
+        cq.Workplane("YZ")
+        .rect(usb_y, usb_z)
+        .extrude(wall_thickness * 3, both=True)
+        .edges("|X")
+        .fillet(usb_r)
+        .translate((-side_wall_x, 0, usb_x + (usb_z / 2)))
+    )
 
+def make_pin_holes():
+    full_height = bottom_z + top_z + m
+    return (
+        cq.Workplane("XY")
+        .box(pin_hole_x, pin_hole_y, full_height, centered=c)
+        .translate((pin_hole_x_offset, pin_y_offset, 0))
+        .union(
+            cq.Workplane("XY")
+            .box(pin_hole_x, pin_hole_y, full_height, centered=c)
+            .translate((pin_hole_x_offset, -pin_y_offset, 0))
+        )
+    )
+
+bottom_plate = cq.Workplane("XY").box(case_x, case_y, bottom_z, centered=c)
+top_plate = cq.Workplane("XY").box(case_x, case_y, top_z, centered=c).translate((0, 0, bottom_z))
 left_wall = make_short_wall(-side_wall_x)
 right_wall = make_short_wall(side_wall_x)
 front_wall = make_long_wall(front_y)
 back_wall = make_long_wall(back_y)
 front_gap = make_gap(front_y)
 back_gap = make_gap(back_y)
-
-usb_wole = (
-    cq.Workplane("YZ")
-    .rect(usb_w, usb_h)
-    .extrude(wall_thickness * 3, both=True)
-    .edges("|X")
-    .fillet(usb_r)
-    .translate((-side_wall_x, 0, usb_z + (usb_h / 2)))
-)
+front_rib = make_crush_rib(rib_inner_front_y)
+back_rib = make_crush_rib(rib_inner_back_y)
+usb_yole = make_usb_hole()
+pin_hole_tool = make_pin_holes()
 
 case = (
     bottom_plate
@@ -78,20 +99,10 @@ case = (
     .union(right_wall)
     .union(front_wall)
     .union(back_wall)
-    #.cut(front_gap)
-    #.cut(back_gap)
-    .cut(usb_wole)
-)
-
-case = (
-    case.faces(">Z")
-    .workplane()
-    .pushPoints([
-        (pin_hole_x_offset, pin_y_offset),
-        (pin_hole_x_offset, -pin_y_offset)
-    ])
-    .rect(pin_hole_x, pin_hole_y)
-    .cutThruAll()
+    .union(front_rib)
+    .union(back_rib)
+    .cut(usb_yole)
+    .cut(pin_hole_tool)
 )
 
 cq.exporters.export(case, "protector.stl")
