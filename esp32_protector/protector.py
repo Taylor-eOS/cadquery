90964b0a1087565f7b3fc691771e16587e6d0984
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-05-15 19:32:07 +0200

    Pre wall protrusion version
    With USB hole. Walls are placed outside the perimeter of the PCB rather than cutting into or reducing its area.

--- esp32_protector/protector.py
+++ esp32_protector/protector.py
@@ -7,47 +7,51 @@ bottom_z = 5.3
 top_height = 2.7
 wall_z = bottom_z + top_height
 wall_thickness = 1.0
-side_wall_x = (pcb_x / 2) - (wall_thickness / 2)
-front_y = (pcb_y / 2) - (wall_thickness / 2)
-back_y = -(pcb_y / 2) + (wall_thickness / 2)
+case_x = pcb_x + (2 * wall_thickness)
+case_y = pcb_y + (2 * wall_thickness)
+side_wall_x = (pcb_x / 2) + (wall_thickness / 2)
+front_y = (pcb_y / 2) + (wall_thickness / 2)
+back_y = -(pcb_y / 2) - (wall_thickness / 2)
 gap_width = 8.0
-full_wall_length = pcb_x - (2 * wall_thickness)
 c = (True, True, False)
 pin_hole_x = 20.0 + (2 * m)
 pin_hole_y = 2.4 + (2 * m)
 pin_hole_x_offset = -0.4
-pin_hole_center_dist = 12.5
-pin_y_offset = (pcb_y / 2) - wall_thickness - (pin_hole_y / 2)
+pin_y_offset = (pcb_y / 2) - (pin_hole_y / 2)
+usb_h = 2.4 + (2 * m)
+usb_w = 8.3 + (2 * m)
+usb_r = (usb_h / 2) - 0.01
+usb_z = wall_z + 0.7
 
 def make_short_wall(tx):
     return (
         cq.Workplane("XY")
-        .box(wall_thickness, pcb_y, top_height, centered=c)
+        .box(wall_thickness, case_y, top_height, centered=c)
         .translate((tx, 0, wall_z))
     )
 
 def make_long_wall(ty):
     return (
         cq.Workplane("XY")
-        .box(full_wall_length, wall_thickness, top_height, centered=c)
+        .box(pcb_x, wall_thickness, top_height, centered=c)
         .translate((0, ty, wall_z))
     )
 
 def make_gap(ty):
     return (
         cq.Workplane("XY")
-        .box(gap_width, wall_thickness, top_height * 2, centered=c)
+        .box(gap_width, wall_thickness * 3, top_height * 2, centered=c)
         .translate((0, ty, bottom_z))
     )
 
 bottom_plate = (
     cq.Workplane("XY")
-    .box(pcb_x, pcb_y, bottom_z, centered=c)
+    .box(case_x, case_y, bottom_z, centered=c)
 )
 
 top_plate = (
     cq.Workplane("XY")
-    .box(pcb_x, pcb_y, top_height, centered=c)
+    .box(case_x, case_y, top_height, centered=c)
     .translate((0, 0, bottom_z))
 )
 
@@ -58,6 +62,15 @@ back_wall = make_long_wall(back_y)
 front_gap = make_gap(front_y)
 back_gap = make_gap(back_y)
 
+usb_wole = (
+    cq.Workplane("YZ")
+    .rect(usb_w, usb_h)
+    .extrude(wall_thickness * 3, both=True)
+    .edges("|X")
+    .fillet(usb_r)
+    .translate((-side_wall_x, 0, usb_z + (usb_h / 2)))
+)
+
 case = (
     bottom_plate
     .union(top_plate)
@@ -65,8 +78,9 @@ case = (
     .union(right_wall)
     .union(front_wall)
     .union(back_wall)
-    .cut(front_gap)
-    .cut(back_gap)
+    #.cut(front_gap)
+    #.cut(back_gap)
+    .cut(usb_wole)
 )
 
 case = (
