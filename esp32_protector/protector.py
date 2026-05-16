31918afcdaaa188ca5599d996f3e202feb6bddf6
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-05-16 07:37:31 +0200

    Code structure housekeeping
    -Derived pin hole X from distance to the walls.
    -Replaced reuse of the hardcoded margin parameter `m` with separate values.
    -Modified the pin hole Z cutting logic to cut down from the base plate rather than outwards from the center.
    -Assembled discrete sub-components before generating the final combined model.
    -Renamed specific instance variables and functions to emphasize structural intent.
    -Consistency in creating single objects.

--- esp32_protector/protector.py
+++ esp32_protector/protector.py
@@ -1,29 +1,34 @@
 import cadquery as cq
 
-m = 0.1
-pcb_x = 23.0 + (2 * m)
-pcb_y = 18.0 + (2 * m)
+PCB_CLEARANCE = 0.1
+pcb_x = 23.0 + (2 * PCB_CLEARANCE)
+pcb_y = 18.0 + (2 * PCB_CLEARANCE)
 plate_z = 8.0
 wall_thickness = 1.0
-case_x = pcb_x + (2 * wall_thickness)
-case_y = pcb_y + (2 * wall_thickness)
+e = 0.01
+case_x = pcb_x + (wall_thickness * 2)
+case_y = pcb_y + (wall_thickness * 2)
 side_wall_x = (pcb_x / 2) + (wall_thickness / 2)
 front_y = (pcb_y / 2) + (wall_thickness / 2)
 back_y = -(pcb_y / 2) - (wall_thickness / 2)
 c = (True, True, False)
-pin_hole_x = 20.0 + (2 * m)
-pin_hole_y = 2.4 + (2 * m)
-pin_hole_x_offset = -0.7
+PIN_CLEARANCE = 0.15
+pin_hole_y = 2.4 + (2 * PIN_CLEARANCE)
+pin_dist_left = 1.0
+pin_dist_right = 2.0
+pin_hole_x = pcb_x - pin_dist_left - pin_dist_right
+pin_hole_x_offset = (pin_dist_left - pin_dist_right) / 2
 pin_y_offset = (pcb_y / 2) - (pin_hole_y / 2)
-usb_z = 3.0 + (2 * m)
-usb_y = 8.8 + (2 * m)
-usb_r = (usb_z / 2) - m
+USB_CLEARANCE = 0.08
+usb_z = 3.0 + (2 * USB_CLEARANCE)
+usb_y = 8.8 + (2 * USB_CLEARANCE)
+usb_r = (usb_z / 2) - e
 usb_x = plate_z + 0.7
 rib_x = 10.0
-rib_y = 0.2
+rib_y = (2 * 0.1)
 rib_z = 2.7
-rib_inner_front_y = (pcb_y / 2) - (rib_y / 2)
-rib_inner_back_y = -(pcb_y / 2) + (rib_y / 2)
+rib_front_y = (pcb_y / 2) - (rib_y / 2)
+rib_back_y = -(pcb_y / 2) + (rib_y / 2)
 
 def make_short_wall(tx):
     return (
@@ -39,20 +44,16 @@ def make_long_wall(ty):
         .translate((0, ty, plate_z))
     )
 
-def make_pin_holes():
-    full_height = plate_z + rib_z + m
+def make_pin_hole(ty):
+    tool_height = plate_z + (2 * e)
+    z_start = plate_z + e
     return (
         cq.Workplane("XY")
-        .box(pin_hole_x, pin_hole_y, full_height, centered=c)
-        .translate((pin_hole_x_offset, pin_y_offset, 0))
-        .union(
-            cq.Workplane("XY")
-            .box(pin_hole_x, pin_hole_y, full_height, centered=c)
-            .translate((pin_hole_x_offset, -pin_y_offset, 0))
-        )
+        .box(pin_hole_x, pin_hole_y, tool_height, centered=c)
+        .translate((pin_hole_x_offset, ty, z_start - tool_height))
     )
 
-def make_usb():
+def make_usb_hole():
     return (
         cq.Workplane("YZ")
         .rect(usb_y, usb_z)
@@ -82,21 +83,42 @@ left_wall = make_short_wall(-side_wall_x)
 right_wall = make_short_wall(side_wall_x)
 front_wall = make_long_wall(front_y)
 back_wall = make_long_wall(back_y)
-front_rib_assembly = make_supported_rib(rib_inner_front_y, front_y - (wall_thickness / 2), -1.0)
-back_rib_assembly = make_supported_rib(rib_inner_back_y, back_y + (wall_thickness / 2), 1.0)
-usb_hole = make_usb()
-pin_hole_tool = make_pin_holes()
+front_rib = make_supported_rib(rib_front_y, front_y - (wall_thickness / 2), -1.0)
+back_rib = make_supported_rib(rib_back_y, back_y + (wall_thickness / 2), 1.0)
+usb_hole = make_usb_hole()
+pin_hole_front = make_pin_hole(pin_y_offset)
+pin_hole_back = make_pin_hole(-pin_y_offset)
+
+pin_holes = (
+    pin_hole_front.union(pin_hole_back)
+)
+
+bottom_panel = (
+    base_plate
+    .cut(pin_holes)
+)
+
+usb_wall = (
+    left_wall
+    .cut(usb_hole)
+)
+
+long_walls = (
+    front_wall
+    .union(back_wall)
+    .union(front_rib)
+    .union(back_rib)
+)
+
+walls = (
+    long_walls
+    .union(usb_wall)
+    .union(right_wall)
+)
 
 case = (
-    base_plate
-    .union(left_wall)
-    .union(right_wall)
-    .union(front_wall)
-    .union(back_wall)
-    .cut(pin_hole_tool)
-    .union(front_rib_assembly)
-    .union(back_rib_assembly)
-    .cut(usb_hole)
+    bottom_panel
+    .union(walls)
 )
 
 cq.exporters.export(case, "protector.stl")
