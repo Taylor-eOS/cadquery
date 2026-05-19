ab01fa035ab074b27c6c8c88534b714c6deb7a9b
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-05-19 20:07:39 +0200

    With triangle ledge

--- solar_box/solar.py
+++ solar_box/solar.py
@@ -4,9 +4,9 @@ width = 52.0
 depth = 34.0
 height = 12.0
 wall = 0.8
-ledge_height = 3.0
-ledge_width = 47.0
-ledge_depth = 30.0
+ledge_drop = 3.1
+ledge_length = 2.4
+z = (height / 2) - ledge_drop
 
 box = (
     cq.Workplane("XY")
@@ -15,14 +15,31 @@ box = (
     .shell(-wall)
 )
 
-box = (
-    box
-    .faces(">Z")
-    .workplane()
-    .workplane(offset=-((height / 2) - ledge_height))
-    .rect(width - 2 * wall, depth - 2 * wall)
-    .rect(ledge_width, ledge_depth)
-    .extrude(wall)
+left_support = (
+    cq.Workplane("XZ")
+    .polyline([
+        (-width / 2 + wall, z),
+        (-width / 2 + wall + ledge_length, z),
+        (-width / 2 + wall, z - ledge_length),
+    ])
+    .close()
+    .extrude(depth - 2 * wall)
+    .translate((0, depth / 2 - wall, 0))
 )
+
+right_support = (
+    cq.Workplane("XZ")
+    .polyline([
+        (width / 2 - wall, z),
+        (width / 2 - wall - ledge_length, z),
+        (width / 2 - wall, z - ledge_length),
+    ])
+    .close()
+    .extrude(depth - 2 * wall)
+    .translate((0, depth / 2 - wall, 0))
+)
+
+box = box.union(left_support)
+box = box.union(right_support)
 
 cq.exporters.export(box, "solar.stl")
