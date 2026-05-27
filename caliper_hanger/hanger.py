1c828e6f9965ded7cacb164e5d810221df4c1926
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-05-27 17:06:51 +0200

    Three-piece concept

--- caliper_hanger/hanger.py
+++ caliper_hanger/hanger.py
@@ -1,31 +1,20 @@
 import cadquery as cq
 
-base_width = 54.0
-base_height = 9.0
-base_thickness = 4.0
-pin_radius = 4.0
-pin_length = 11.0
-arm_spacing = 45.0
-arm_radius = 3.0
-arm_length = 20.0
+base_x = 46.0
+base_y = 9.0
+base_z = 3.8
+pin_radius = 2.3 #For 5mm shelf
+pin_length = 12.0 + base_z
 tip_radius = 1.0
-clearance = 0.1
+clearance = 0.5
+hole_radius = pin_radius + clearance
+arm_spacing = base_x - (hole_radius * 2) - ((base_y - hole_radius) / 2)
 
 base = (
     cq.Workplane("XY")
-    .box(base_width, base_height, base_thickness)
+    .box(base_x, base_y, base_z)
     .edges("|Z")
     .fillet(3)
-)
-
-base = (
-    base
-    .faces("<Z")
-    .workplane()
-    .circle(pin_radius)
-    .extrude(pin_length)
-    .faces("<Z")
-    .fillet(tip_radius)
 )
 
 base = (
@@ -36,17 +25,25 @@ base = (
         (-arm_spacing / 2, 0),
         (arm_spacing / 2, 0)
     ])
-    .circle(arm_radius + clearance)
-    .cutBlind(-(base_thickness - 1.0))
+    .circle(hole_radius)
+    .cutThruAll()
 )
 
-arm = (
+base = (
+    base
+    .faces(">Z")
+    .workplane()
+    .circle(hole_radius)
+    .cutThruAll()
+)
+
+pin = (
     cq.Workplane("XY")
-    .circle(arm_radius)
-    .extrude(arm_length + base_thickness - 1.0)
+    .circle(pin_radius)
+    .extrude(pin_length + base_z)
     .faces(">Z")
     .fillet(tip_radius)
 )
 
 cq.exporters.export(base, "base.stl")
-cq.exporters.export(arm, "arm.stl")
+cq.exporters.export(pin, "pin.stl")
