6eeffe62710c9faab7adaea200026c8bf99b8fb9
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-05-27 15:54:24 +0200

    Two-piece design

--- caliper_hanger/hanger.py
+++ caliper_hanger/hanger.py
@@ -9,16 +9,17 @@ arm_spacing = 45.0
 arm_radius = 3.0
 arm_length = 20.0
 tip_radius = 1.0
+clearance = 0.1
 
-hanger = (
+base = (
     cq.Workplane("XY")
     .box(base_width, base_height, base_thickness)
     .edges("|Z")
     .fillet(3)
 )
 
-hanger = (
-    hanger
+base = (
+    base
     .faces("<Z")
     .workplane()
     .circle(pin_radius)
@@ -27,18 +28,25 @@ hanger = (
     .fillet(tip_radius)
 )
 
-hanger = (
-    hanger
+base = (
+    base
     .faces(">Z")
     .workplane()
     .pushPoints([
         (-arm_spacing / 2, 0),
         (arm_spacing / 2, 0)
     ])
+    .circle(arm_radius + clearance)
+    .cutBlind(-(base_thickness - 1.0))
+)
+
+arm = (
+    cq.Workplane("XY")
     .circle(arm_radius)
-    .extrude(arm_length)
+    .extrude(arm_length + base_thickness - 1.0)
     .faces(">Z")
     .fillet(tip_radius)
 )
 
-cq.exporters.export(hanger, "hanger.stl")
+cq.exporters.export(base, "base.stl")
+cq.exporters.export(arm, "arm.stl")
