6492350a22f8923da7e74328f2a138b558cc94e1
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2026-06-04 13:47:03 +0200

    Actual values

--- candle_shell/candle.py
+++ candle_shell/candle.py
@@ -1,9 +1,10 @@
 import cadquery as cq
 
 battery_diameter = 18.4
+extra_diameter = 2.7
+shell_radius = (battery_diameter + extra_diameter) / 2
 battery_height = 65.3
-shell_radius = (battery_diameter + 2.8) / 2
-shell_z = battery_height + 8.0
+extra_length = 23.7
 shell_thickness = 0.8
 filament_hole_diameter = 3
 EPS = 0.01
@@ -11,7 +12,7 @@ EPS = 0.01
 candle = (
     cq.Workplane("XY")
     .circle(shell_radius)
-    .extrude(shell_z)
+    .extrude(battery_height + extra_length)
     .faces(">Z")
     .shell(-shell_thickness)
 )
